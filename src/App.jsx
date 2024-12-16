4dfc066d5b81200a4da3f8b8d3915333b139e8b9
kaijuxkitty <kaijuxkitty@gmail.com>
2024-12-16 10:25:20 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Authenticate from "./components/Authenticate"
+import SignUpForm from "./components/SignUpForm"
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <>
+     <Authenticate/>
+     <SignUpForm/>
+    </>
+  )
+}
+
+export default App
